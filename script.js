1926c68e9660277b32bd50de3463b6ed45ca0376
droidDev-Ak <programcoder.ak@gmail.com>
2025-05-30 14:09:03 +0530

    Only functionality left

--- script.js
+++ script.js
@@ -1,143 +1,170 @@
 //Basic operations 
 let string = "";
-let exp=true;
+let exp = true;
 const buttons = document.querySelectorAll(".btn");
-const output = document.querySelector(".output"); 
-let arr=[];
-let i=0;
-if(window.innerWidth > 450){
-    document.querySelector(".mobile-functions").classList.add("inactive");}
+const output = document.querySelector(".output");
+let arr = [];
+let i = 0;
+let mobile = false;
+let historyy = false;
+
+console.log(window.innerWidth);
+
+if (window.innerWidth > 390 || window.innerWidth > 450) {
+    document.querySelector(".mobile-functions").classList.add("inactive");
+    mobile = true;
+    console.log("1");
+
+}
+function history() {
+    console.log("history");
+    historyy = true;
+    console.log("2");
+
+    document.querySelector(".history").classList.toggle("inactive");
+}
+
+
+
+
+document.body.addEventListener('touchstart', (e) => {
+    touchStartX = e.touches[0].clientX;
+    touchStartY = e.touches[0].clientY;
+    if (mobile && historyy) {
+        if (touchStartX < 100 || touchStartX > 250) {
+            document.querySelector(".history").classList.toggle("inactive");
+
+        }
+    }
+
+})
 
 document.querySelector("#equals").addEventListener("click", () => {
-    arr.push(string+"="+eval(string));
+    arr.push(string + "=" + eval(string));
     console.log(arr);
-    document.querySelector(".history-list").innerHTML+=`<li>${arr[i]}</li>`;
+    document.querySelector(".history-list").innerHTML += `<li>${arr[i]}</li>`;
     i++;
 });
-document.querySelector(".close").addEventListener("click",()=>{
-document.querySelector(".history").classList.toggle("inactive");
-document.querySelector('.dropdown').value = '0';
+document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".history").classList.toggle("inactive");
+    document.querySelector('.dropdown').value = '0';
 })
 
 
 buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    let buttonText = e.target.innerHTML;
-    if(buttonText === "x") {
-        buttonText="*";
-    }
-    else if(buttonText === "÷") {
-        buttonText="/";
-    }
+    button.addEventListener("click", (e) => {
+        let buttonText = e.target.innerHTML;
+        if (buttonText === "x") {
+            buttonText = "*";
+        }
+        else if (buttonText === "÷") {
+            buttonText = "/";
+        }
 
 
-    console.log(buttonText); 
-    
-    if (buttonText === "=") {
-      string = eval(string);
-        if(string!=string){
+        console.log(buttonText);
 
-            output.value="Not defined";
+        if (buttonText === "=") {
+            string = eval(string);
+            if (string != string) {
+
+                output.value = "Not defined";
+
+            }
+            else {
+
+                output.value = string;
+            }
+        }
+        else if (buttonText == "%") {
+            string = eval(string) / 100;
 
         }
-        else{
+        else if (buttonText === "+/-") {
+            if (string.startsWith("-")) {
+                string = string.slice(1);
+
+            }
+            else {
+                string = "-" + string;
+            }
 
             output.value = string;
         }
-    }
-    else if(buttonText=="%"){
-        string = eval(string) / 100;
+        else {
+            if (buttonText === "AC") {
+                string = "";
+                output.value = string;
+            }
+            else {
 
-    }
-    else if(buttonText === "+/-"){
-        if(string.startsWith("-")){
-            string = string.slice(1); 
-
+                string += buttonText;
+                output.value = string;
+            }
         }
-        else{
-            string = "-" + string; 
-        }
-
-        output.value = string; 
-    } 
-    else {
-        if(buttonText === "AC") {
-            string="";
-            output.value = string; 
-        }
-        else{
-
-            string += buttonText;
-            output.value = string; 
-        }
-    }
-  });
+    });
 });
 
-function history(){
-    console.log("history");
-    
-document.querySelectorAll(".history").classList.toggle("inactive");
-}
 
-function expand(){
+
+function expand() {
     console.log("expand");
-   
+
     document.querySelector(".container").classList.toggle("container-expand");
 
     document.querySelector(".functions").classList.toggle("functions-expand");
     document.querySelector(".extra-functions").classList.toggle("inactive");
-    
-document.querySelector('.dropdown').value = '0';
+
+    document.querySelector('.dropdown').value = '0';
 
 }
 const dropdown = document.querySelector(".dropdown");
 
-dropdown.addEventListener("change",(e)=>{
+dropdown.addEventListener("change", (e) => {
     console.log(e.target.value);
-    if(e.target.value=="1"){
+    if (e.target.value == "1") {
         history();
     }
 
-    else if(e.target.value=="2"){
+    else if (e.target.value == "2") {
         expand();
     }
-    
+
 })
 
 // For Swipe for mobile phones
 
 
 
- const first=document.querySelector('.container');
+const first = document.querySelector('.container');
 
-        let touchStartX = 0;
-        let touchStartY = 0;
-        let touchEndX = 0;
-        let touchEndY = 0;
+let touchStartX = 0;
+let touchStartY = 0;
+let touchEndX = 0;
+let touchEndY = 0;
 
-        first.addEventListener('touchstart',(e)=>{
-        if(e.touches.length>1) return; // Ignore multi-touch
-          touchStartX = e.touches[0].clientX;
-            touchStartY = e.touches[0].clientY;
-            console.log("Touch started at: ", touchStartX, touchStartY);
-            
-
-        })       
-        first.addEventListener('touchend',(e)=>{
-            if(e.touches.length>1) return;
-            touchEndX=e.changedTouches[0].clientX;
-            touchEndY=e.changedTouches[0].clientY;
+first.addEventListener('touchstart', (e) => {
+    if (e.touches.length > 1) return; // Ignore multi-touch
+    touchStartX = e.touches[0].clientX;
+    touchStartY = e.touches[0].clientY;
+    console.log("Touch started at: ", touchStartX, touchStartY);
 
 
-            const delX= touchEndX - touchStartX;
-            const delY= touchEndY - touchStartY;
-            if(Math.abs(delX) > Math.abs(delY)){
+})
+first.addEventListener('touchend', (e) => {
+    if (e.touches.length > 1) return;
+    touchEndX = e.changedTouches[0].clientX;
+    touchEndY = e.changedTouches[0].clientY;
 
-                if(delX > 0){
-                    document.querySelector(".functions").classList.toggle("inactive");
-                    document.querySelector(".mobile-functions").classList.toggle("inactive");
 
-    } 
-            }
-        })
+    const delX = touchEndX - touchStartX;
+    const delY = touchEndY - touchStartY;
+    if (Math.abs(delX) > Math.abs(delY)) {
+
+        if (delX > 0) {
+            document.querySelector(".functions").classList.toggle("inactive");
+            document.querySelector(".mobile-functions").classList.toggle("inactive");
+
+        }
+    }
+})
